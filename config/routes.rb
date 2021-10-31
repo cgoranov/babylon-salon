cb550a3106e3424a43bd49be7bef550ad09c39fc
Che <cgoranov@gmail.com>
2021-10-31 18:50:10 -0500

    added appointments routes nested in users routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
 
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
 
-  resources :users
+  resources :users do 
+    resources :appointments
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
