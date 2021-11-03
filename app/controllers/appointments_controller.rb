cb603dd4395f44693b4e9b9007c606da3f64ee06
Che <cgoranov@gmail.com>
2021-11-03 17:55:07 -0500

    Appointments controller buildout with new and index

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,7 +1,12 @@
 class AppointmentsController < ApplicationController
 
+    def index
+        @user = params[:user_id]    
+    end
+
     def new
-        
+        @user = params[:user_id]
+        @barbers = Barber.all
     end
 
 end
