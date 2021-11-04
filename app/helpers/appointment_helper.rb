e5f567fb0827481da1ddb09be81de04d45e0b362
Che <cgoranov@gmail.com>
2021-11-03 19:53:16 -0500

    new appointment form buildout

--- app/helpers/appointment_helper.rb
+++ app/helpers/appointment_helper.rb
@@ -4,12 +4,12 @@ module AppointmentHelper
         @time_slots = ["9 am - 10 am", "10 am - 11 am", "11 am - 12 pm", "12 pm - 1 pm", "1 pm - 2 pm", "2 pm - 3 pm", "3 pm - 4 pm", "4 pm - 5 pm", "5 pm - 6 pm", "6 pm - 7 pm"]
     end
 
-    def dates_slots(days_out)
+    def date_slots(days_out)
 
-        @dates_slots = [Time.now]
+        @dates_slots = [Time.now.strftime("%A - %B %d")]
         i = 1
         days_out.times do 
-           @dates_slots << Time.now + i.day 
+           @dates_slots << (Time.now + i.day).strftime("%A - %B %d")
            i += 1
         end
         @dates_slots
