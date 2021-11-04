674498fc4391e5a373f57835c4feb4efdfe85991
Che <cgoranov@gmail.com>
2021-11-03 19:18:47 -0500

    Appointment Helper build out for time_slots and date_slots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
     include ApplicationHelper
-    include CalendarHelper
+    include AppointmentHelper
     protect_from_forgery with: :exception
 end
---
+++ app/helpers/appointment_helper.rb
@@ -0,0 +1,17 @@
+module AppointmentHelper
+
+    def time_slots
+        @time_slots = ["9 am - 10 am", "10 am - 11 am", "11 am - 12 pm", "12 pm - 1 pm", "1 pm - 2 pm", "2 pm - 3 pm", "3 pm - 4 pm", "4 pm - 5 pm", "5 pm - 6 pm", "6 pm - 7 pm"]
+    end
+
+    def dates_slots(days_out)
+
+        @dates_slots = [Time.now]
+        i = 1
+        days_out.times do 
+           @dates_slots << Time.now + i.day 
+           i += 1
+        end
+        @dates_slots
+    end
+end
