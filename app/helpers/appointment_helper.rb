33e6d4ecdee365d6b402c7b87e288d86478690cd
Che <cgoranov@gmail.com>
2021-11-04 22:47:48 -0500

    updated AppointmentHelper methods for edit form

--- app/helpers/appointment_helper.rb
+++ app/helpers/appointment_helper.rb
@@ -4,6 +4,10 @@ module AppointmentHelper
         @time_slots = ["9 am - 10 am", "10 am - 11 am", "11 am - 12 pm", "12 pm - 1 pm", "1 pm - 2 pm", "2 pm - 3 pm", "3 pm - 4 pm", "4 pm - 5 pm", "5 pm - 6 pm", "6 pm - 7 pm"]
     end
 
+    def edit_time_slots
+        time_slots.unshift "(current time slot) #{@appointment.full_date.strftime("%l:%M %p")} - #{@appointment.end_time.strftime("%l:%M %p")}"
+    end
+
     def date_slots(days_out)
         @dates_slots = [Time.now.strftime('%A - %B %d')]
         i = 1
@@ -12,6 +16,11 @@ module AppointmentHelper
            i += 1
         end
         @dates_slots
+    end
+
+    def edit_date_slots(days_out)
+        edit_date_slots = date_slots(days_out)
+        edit_date_slots.unshift "(current date) #{@appointment.full_date.strftime('%A - %B %d')}"
     end
 
 end
