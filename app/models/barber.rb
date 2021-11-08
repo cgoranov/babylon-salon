cb606f75bda6d79ccd86d19f2bf86662d3ef6520
Che <cgoranov@gmail.com>
2021-11-08 15:49:48 -0600

    add barber with most appointments scope macro only as well

--- app/models/barber.rb
+++ app/models/barber.rb
@@ -1,12 +1,13 @@
 class Barber < ApplicationRecord
     has_many :appointments
     has_one_attached :avatar
-    scope :most_appointments, -> { joins(:appointments).group(:barber_id).count }
+    # scope :most_appointments, -> { joins(:appointments).group(:barber_id).count }
+    scope :most_appointments, -> { find(joins(:appointments).group(:barber_id).order('count_barber_id desc').count('barber_id').first[0]) }
 
-    def self.top_barber
-        top_barbers = Barber.most_appointments.sort_by { |k, v| -v }
-        top_performer = Barber.find_by_id(top_barbers[0][0]).first_name
-        top_performer
-    end
+    # def self.top_barber
+    #     top_barbers = Barber.most_appointments.sort_by { |k, v| -v }
+    #     top_performer = Barber.find_by_id(top_barbers[0][0]).first_name
+    #     top_performer
+    # end
 
 end
